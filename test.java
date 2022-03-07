51bab8264c1c2615ba2d06c34a3c4709509b4e12
DaiCae <925268575@qq.com>
2022-03-07 16:26:19 +0800

    #Update

--- test.java
+++ test.java
@@ -1,25 +1,38 @@
-import java.util.HashMap;
+import java.util.Arrays;
+// import java.util.HashMap;
 
 public class test {
-    public static int lengthOfLongestSubstring(String s) {
-        int start = 0;
-        int end = 0;
-        HashMap<String,Integer> map = new HashMap<String,Integer>();
-        for(int i = 0; i < s.length(); i++) {
-            String c = String.valueOf(s.charAt(i));
-            if(map.containsKey(c)){
-                start = map.get(c) + 1;
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int o1=0, o2=0;
+        int k = (m + n)/2;
+        
+        while(k!=1){
+            int tmp = k/2;
+            if(nums1[o1 + tmp -1]<=nums2[o2 + tmp -1]){
+                o1 +=tmp;
             }else{
-                map.put(c, i);
-                end = i;
+                o2 +=tmp;
             }
+            k = k-k/2;
         }
-        return end - start;
+        // o1 +=1;
+        // o2 +=1;
+        o1 +=0;
+        o2 +=0;
+        if((m+n)%2==0){
+            return (double)(nums1[o1] + nums2[o2])/2;
+        }else{
+            return (double)Math.min(nums1[o1],nums2[o2]);
+        }
     }
 
     public static void main(String[] args){
-        String s = "hello world";
-        int a =lengthOfLongestSubstring(s);
+        int[] nums1 = {1 ,3, 4, 9};
+        int[] nums2 = {1, 2, 3, 3, 5, 6, 7, 8};
+
+        double a =findMedianSortedArrays(nums1, nums2);
         System.out.println(a);
     }
 }
