54dd8a93c8d6f334f4e53bfc0112eca4ba4c699f
DaiCae <925268575@qq.com>
2022-06-16 11:19:40 +0800

    #Update 532

--- test.java
+++ test.java
@@ -1,40 +1,70 @@
-import java.util.HashMap;
+import java.util.*;
 
-// import java.util.Arrays;
-// import java.util.HashMap;
+
+class Solution {
+
+}
 
 public class test {
-    public static int[] platesBetweenCandles(String s, int[][] queries) {
-        int n = s.length();
-        int[] preSum = new int[n];
-        for (int i = 0, sum = 0; i < n; i++) {
-            if (s.charAt(i) == '*') sum++;
-            preSum[i] = sum;
-        }
-        int[] left = new int[n];
-        for (int i = 0, l = -1; i < n; i++) {
-            if (s.charAt(i) == '|') l = i;
-            left[i] = l;
-        }
-        int[] right = new int[n];
-        for (int i = n - 1, r = -1; i >= 0; i--) {
-            if (s.charAt(i) == '|') r = i;
-            right[i] = r;
-        }
-        int[] ans = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int x = right[queries[i][0]], y = left[queries[i][1]];
-            ans[i] = (x == -1 || y == -1 || x >= y) ? 0 : preSum[y] - preSum[x];
-        }
-        return ans;
+    public static void main(String[] args) {
+//        int[] a = {1,1,1,2,1};
+//        1,3,1,5,4
+        int[] a = {1,3,1,5,4};
+        int k = 0;
+        System.out.println(findPairs(a,k));
     }
 
-    public static void main(String[] args){
-        // String s= "**|**|***|";
-        // int[][] a ={{2,5},{5,9}};
-        String s ="***|**|*****|**||**|*";
-        int[][] a ={{1,17},{4,5},{14,17},{5,11},{15,16}};
-        platesBetweenCandles(s, a);
-        System.out.println(a);
+
+    public static int findPairs(int[] nums, int k) {
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> res = new HashSet<>();
+        for (int num : nums) {
+            visited.add(num);
+        }
+        for (int num : nums) {
+            if (visited.contains(num - k)){
+                res.add(num - k);
+            }
+            if (visited.contains(num + k)){
+                res.add(num);
+            }
+        }
+        return res.size();
     }
+//    public static int findPairs(int[] nums, int k) {
+//        int l = nums.length;
+//        int n =0;
+//        HashSet<Integer> h0 = new HashSet<>(); //(i,i+k)
+//        HashSet<Integer> h1 = new HashSet<>(); //(i,i-k)
+//        for(int i = 0; i < l-1; i++){
+//            boolean f0 = false,f1 = false;
+//            for(int j=i+1; j<l; j++){
+//                int diff = nums[i] - nums[j];
+//                if(diff == k){
+//                    f0 = true;
+//                    if(!h0.contains(nums[i])) n++;
+//                    h0.add(nums[i]);
+//                    if((nums[i]-nums[j])==k&&(nums[j]-nums[i])==-k){
+//                        h0.add(nums[j]);
+//                        h1.add(nums[j]);
+//                        h0.add(k);
+//                        h1.add(-k);
+//                    }
+//                }else if ((diff + k)==0){
+//                    f1 = true;
+//                    if(!h1.contains(nums[i])) n++;
+//                    h1.add(nums[i]);
+//                    if((nums[i]-nums[j])==-k&&(nums[j]-nums[i])==k){
+//                        h0.add(nums[j]);
+//                        h1.add(nums[j]);
+//                        h0.add(k);
+//                        h1.add(-k);
+//                    }
+//                }
+//                if(f0 && f1) break;
+//            }
+//        }
+//        return n;
+//    }
 }
+
