de1a230cb0dfac0646a26d6f7f9cbc4dfe7c6b71
DaiCae <925268575@qq.com>
2022-06-19 17:50:02 +0800

    #Update 508

--- test.java
+++ test.java
@@ -1,70 +1,50 @@
 import java.util.*;
 
-
-class Solution {
-
-}
-
 public class test {
     public static void main(String[] args) {
 //        int[] a = {1,1,1,2,1};
 //        1,3,1,5,4
-        int[] a = {1,3,1,5,4};
-        int k = 0;
-        System.out.println(findPairs(a,k));
     }
 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-    public static int findPairs(int[] nums, int k) {
-        Set<Integer> visited = new HashSet<>();
-        Set<Integer> res = new HashSet<>();
-        for (int num : nums) {
-            visited.add(num);
+        TreeNode() {
         }
-        for (int num : nums) {
-            if (visited.contains(num - k)){
-                res.add(num - k);
-            }
-            if (visited.contains(num + k)){
-                res.add(num);
-            }
+
+        TreeNode(int val) {
+            this.val = val;
         }
-        return res.size();
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
-//    public static int findPairs(int[] nums, int k) {
-//        int l = nums.length;
-//        int n =0;
-//        HashSet<Integer> h0 = new HashSet<>(); //(i,i+k)
-//        HashSet<Integer> h1 = new HashSet<>(); //(i,i-k)
-//        for(int i = 0; i < l-1; i++){
-//            boolean f0 = false,f1 = false;
-//            for(int j=i+1; j<l; j++){
-//                int diff = nums[i] - nums[j];
-//                if(diff == k){
-//                    f0 = true;
-//                    if(!h0.contains(nums[i])) n++;
-//                    h0.add(nums[i]);
-//                    if((nums[i]-nums[j])==k&&(nums[j]-nums[i])==-k){
-//                        h0.add(nums[j]);
-//                        h1.add(nums[j]);
-//                        h0.add(k);
-//                        h1.add(-k);
-//                    }
-//                }else if ((diff + k)==0){
-//                    f1 = true;
-//                    if(!h1.contains(nums[i])) n++;
-//                    h1.add(nums[i]);
-//                    if((nums[i]-nums[j])==-k&&(nums[j]-nums[i])==k){
-//                        h0.add(nums[j]);
-//                        h1.add(nums[j]);
-//                        h0.add(k);
-//                        h1.add(-k);
-//                    }
-//                }
-//                if(f0 && f1) break;
-//            }
-//        }
-//        return n;
-//    }
+
+    int maxValue;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    public int[] findFrequentTreeSum(TreeNode root) {
+        dfs(root);
+        ArrayList<Integer> list = new ArrayList<>();
+        map.forEach((key,value)->{
+            if (value == maxValue) list.add(key);
+        });
+        int[] results = new int[list.size()];
+        for (int i = 0; i < results.length; i++) results[i] = list.get(i);
+        return results;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int sum = node.val + dfs(node.left) + dfs(node.right);
+        int newValue = map.getOrDefault(sum,0) + 1;
+        map.put(sum, newValue);
+        maxValue = Math.max(newValue, maxValue);
+        return sum;
+    }
 }
 
